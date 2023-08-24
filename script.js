4c43961219959bdd463b36fab9943a040af45c28
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-08-24 09:15:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
-const parseCode = (str) => {
-  // your code here
-};
+class OutOfRangeError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'OutOfRangeError';
+  }
+}
 
-// Do not change the code below
-const str = prompt("Enter str: ");
-alert(JSON.stringify(parseCode(str)));
+class InvalidExprError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'InvalidExprError';
+  }
+}
+
+function evalString(expr) {
+  try {
+    // Check for invalid operator combinations
+    if (/\+\+|--|\+\-|-\+|\*\/|\/\*/.test(expr)) {
+      throw new InvalidExprError('Expression should not have an invalid combination of operators');
+    }
+
+    // Check for invalid start and end operators
+    if (/^[+/*]/.test(expr)) {
+      throw new SyntaxError('Expression should not start with invalid operator');
+    }
+    if (/[\+\-*/]$/.test(expr)) {
+      throw new SyntaxError('Expression should not end with invalid operator');
+    }
+
+    // Check for valid expression format
+    if (!/^[-+*/\d\s()]+$/.test(expr)) {
+      throw new OutOfRangeError('Expression should only consist of integers and +-/* characters');
+    }
+
+    // Evaluate the expression
+    return eval(expr);
+  } catch (error) {
+    throw error;
+  }
+}
